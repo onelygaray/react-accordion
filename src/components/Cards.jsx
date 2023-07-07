721467eb31044aef5adda67939245a76ee816c46
Onely Garay <onelygaray2002@gmail.com>
2023-07-06 23:23:19 -0600

    create const cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Image1 from "../assets/canada.jpg";
+import Image2 from "../assets/usa.jpg";
+import Image3 from "../assets/españa.jpg";
+import Image4 from "../assets/rusia.jpg";
+
+const Cards = [
+    {
+        header: "Canada",
+        image: Image1,
+        text: 'image description here'
+    },
+    {
+        header: "USA",
+        image: Image2,
+        text: 'image description here'
+    },
+    {
+        header: "España",
+        image: Image3,
+        text: 'image description here'
+    },
+    {
+        header: "Rusia",
+        image: Image4,
+        text: 'image description here'
+    }
+        
+]
